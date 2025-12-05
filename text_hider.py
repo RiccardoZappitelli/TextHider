b65969711dd3f0e4d3516ce02209635a33d03e42
RiccardoZappitelli <zappitelliriccardo@gmail.com>
2025-12-05 15:49:35 +0100

    First Commit

---
+++ text_hider.py
@@ -0,0 +1,29 @@
+bin_to_hidden = {
+    "1":"\u200B",
+    "0":"\u200C",
+}
+hidden_to_bin = {
+    "\u200B":"1",
+    "\u200C":"0"
+}
+
+def encoder(text: str) -> str:
+    res = ""
+    binary_text = ''.join(format(b, '08b') for b in text.encode('utf-8'))
+    for bit in binary_text:
+        res+=bin_to_hidden[bit]
+    return res
+
+def decoder(text: str) -> str:
+    binary = "".join([ hidden_to_bin[x] for x in text])
+    text = ''.join(
+        chr(int(binary[i:i+8], 2))
+        for i in range(0, len(binary), 8)
+    )
+    return text 
+
+if __name__ == "__main__":
+    encoded = encoder(input("Input: "))
+    print(f"=== Encoded ===\n{encoded}\n=== End Encoded ===")
+    decoded = decoder(encoded)
+    print(f"=== Decoded ===\n{decoded}\n=== End Decoded ===")
