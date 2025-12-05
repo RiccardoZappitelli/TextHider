db2dd576b22aa4e5df5fa07bb7a50ea6c767f961
RiccardoZappitelli <zappitelliriccardo@gmail.com>
2025-12-05 16:13:09 +0100

    Choosing start and ending of the hidden text

--- text_hider.py
+++ text_hider.py
@@ -1,3 +1,6 @@
+START = "\u200D"
+END = "\u2060"
+
 bin_to_hidden = {
     "1":"\u200B",
     "0":"\u200C",
@@ -7,15 +10,19 @@ hidden_to_bin = {
     "\u200C":"0"
 }
 
-def encoder(text: str) -> str:
-    res = ""
+def encode(text: str) -> str:
+    res = START
     binary_text = ''.join(format(b, '08b') for b in text.encode('utf-8'))
     for bit in binary_text:
         res+=bin_to_hidden[bit]
-    return res
+    return res+END
 
-def decoder(text: str) -> str:
-    binary = "".join([ hidden_to_bin[x] for x in text])
+def decode(text: str) -> str:
+    start_index = text.index(START)
+    end_index = text.index(END)
+    encoded_text = text[start_index+1:end_index].replace(START, "").replace(END, "")
+
+    binary = "".join([ hidden_to_bin[x] for x in encoded_text])
     text = ''.join(
         chr(int(binary[i:i+8], 2))
         for i in range(0, len(binary), 8)
@@ -23,7 +30,7 @@ def decoder(text: str) -> str:
     return text 
 
 if __name__ == "__main__":
-    encoded = encoder(input("Input: "))
-    print(f"<Encoded>{encoded}<End Encoded>")
-    decoded = decoder(encoded)
-    print(f"<Decoded>{decoded}<End Decoded>") 
+    encoded = encode(input("Input: "))
+    print(f"<Encoded>{encoded}<Encoded>")
+    decoded = decode(encoded)
+    print(f"<Decoded>{decoded}</Decoded>") 
